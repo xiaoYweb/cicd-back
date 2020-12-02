39e3bebd8203187f8169c2e53ed382990b136b5f
y <dota2runa@outlook.com>
2020-12-02 22:06:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+const users = [
+  { name: 'lili', age: 12 },
+  { name: 'leo', age: 12 },
+  { name: 'jojo', age: 12 },
+]
+const server = http.createServer((req, res) => {
+  const { url, } = req;
+  if (url === '/api/users') {
+    res.end(JSON.stringify(users))
+  } else {
+    res.end('Not Found')
+  }
+})
+
+const port = 4000;
+server.listen(port, () => {
+  console.log(`node server is listening port ${port}`)
+})
