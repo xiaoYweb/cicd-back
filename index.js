2cc78f93fa7c56c52833af645cdf4e1ddeb9e25c
yanbinbin <zl@zldeiMac-10.local>
2020-12-03 14:12:27 +0800

    14

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const users = [
   { name: 'lili', age: 20 },
   { name: 'leo', age: 32 },
-  { name: 'jojo', age: 13 },
+  { name: 'jojo', age: 14 },
 ]
 const server = http.createServer((req, res) => {
   const { url, } = req;
